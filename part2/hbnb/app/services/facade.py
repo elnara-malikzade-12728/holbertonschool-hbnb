15d3eb682a91c34b790e626730cb829d1cad5a10
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-02 17:46:45 +0400

    Add task2 paart2

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
@@ -7,10 +8,37 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        """Create a new user and add it to the repository"""
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self,user_id):
+        """Get a user from the repository by id"""
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self,email):
+        """Get a user from the repository by email"""
+        return self.user_repo.get(email)
+
+    def get_all_users(self):
+        """Get all users from the repository"""
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        user = self.get_user(user_id)
+        if not user:
+            return None
+        # Update the attributes
+        user.first_name = user_data.get('first_name', user.first_name)
+        user.last_name = user_data.get('last_name', user.last_name)
+        user.email = user_data.get('email', user.email)
+
+        # Optional: If your repo requires an explicit update call
+        # self.user_repo.update(user_id, user)
+
+        return user
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
